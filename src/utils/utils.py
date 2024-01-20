1d74398ac18ad049423579a51eaebc83c26b9158
THAI HA NGUYEN <nguyenha.mse@gmail.com>
2024-01-20 22:41:49 +0900

    UPDATE utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,11 +2,12 @@ import yaml
 
 
 def load_config(file_path):
+    """Load configuration from a YAML file."""
     try:
         with open(file_path, "r", encoding="utf-8") as file:
-            data = yaml.safe_load(file)
-        return data
-    except FileNotFoundError:
-        raise FileNotFoundError(f"Error: File '{file_path} not found.")
+            config_data = yaml.safe_load(file)
+        return config_data
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"Error: File '{file_path}' not found.") from e
     except yaml.YAMLError as e:
-        raise yaml.YAMLError()
+        raise yaml.YAMLError(f"Error loading YAML file '{file_path}': {e}") from e
